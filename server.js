adde9a4a2fed7b867cdae4382477f82907ee5558
JanTristanH <tristan.heisecke@gmail.com>
2019-08-27 14:10:11 +0200

    removed old unnecessary code

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-console.log('hi');
 const fetch = require('node-fetch');
 const https = require('https');
 const url = "https://geodienste.hamburg.de/HH_WFS_Stadtrad?service=WFS&request=GetFeature&VERSION=1.1.0&typename=stadtrad_stationen";
@@ -21,42 +20,3 @@ fetch(url)
         });
 
     });
-
-let bikeData = () => {
-    return new Promise((resolve, reject) => {
-        const host = "geodienste.hamburg.de";
-        const  path = "HH_WFS_Stadtrad";//?service=WFS&request=GetFeature&VERSION=1.1.0&typename=stadtrad_stationen";
-        const options = {
-            host,
-            port: 443,
-            path,
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/xml',
-                'service' : 'WFS',
-                'request':'GetFeature',
-                'VERSION': '1.1.0',
-                'typename' : 'stadtrad_stationen'
-            }
-        };
-
-        let json = '';
-        const req = https.request(options, (res) => {
-            res.setEncoding('utf8');
-            res.on('data', (chunk) => json += chunk);
-            res.on('end', () => {
-                console.log(`Request to API successful`);
-                resolve(json);//JSON.parse(json));
-            });
-        });
-        req.on('error', (e) => {
-                console.error(`Request to API failed with ${e}`);
-                reject(json);
-            }
-        );
-        //req.write(body);
-        req.end();
-    })
-};
-
-//bikeData().then( res => console.log(res));

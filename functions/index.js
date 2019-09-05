c9ddb63a26f625b08475b059bfcfe0b979ce8ee8
Tristan <tristan.heisecke@gmail.com>
2019-09-05 10:57:25 +0200

    cleaned up comments and variables

--- functions/index.js
+++ functions/index.js
@@ -22,15 +22,13 @@ const functions = require('firebase-functions');
 // Instantiate the Dialogflow client.
 const app = dialogflow({debug: true});
 
-// Handle the Dialogflow intent named 'favorite color'.
-// The intent collects a parameter named 'color'.
-
+// Handle the Dialogflow intent named 'bike available'.
 app.intent('bike available', async (conv) => {
     return new Promise((resolve, reject) =>{
         const fetch = require('node-fetch');
         const parser = require('fast-xml-parser');
         const url = 'https://geodienste.hamburg.de/HH_WFS_Stadtrad?service=WFS&request=GetFeature&VERSION=1.1.0&typename=stadtrad_stationen';
-
+        const stationId = '3CB6C09F1CF83370E57148D538F04E530AC4041D';
         fetch(url)
             .then((response) => {
                 return response.text();
@@ -38,30 +36,23 @@ app.intent('bike available', async (conv) => {
             .then((res) => {
                 let tObj = parser.getTraversalObj(res);
                 let jsonObj = parser.convertToJson(tObj);
-                // eslint-disable-next-line max-len
                 let bikeCount = jsonObj[`wfs:FeatureCollection`][`gml:featureMember`].filter( (e) => {
-                    // eslint-disable-next-line max-len
                     // returns an array with only desired stations, for testing purposes only Sievekingsallee / Sievekingdamm
-                    // eslint-disable-next-line max-len
-                    return e[`app:stadtrad_stationen`][`app:uid`] == '3CB6C09F1CF83370E57148D538F04E530AC4041D';
+                    return e[`app:stadtrad_stationen`][`app:uid`] == stationId;
                 })[0][`app:stadtrad_stationen`][`app:anzahl_raeder`];
-                // Respond with the bike count  and end the conversation.
-                console.log(bikeCount);
-                // eslint-disable-next-line max-len
+                // Respond with the bike count and end the conversation.
                 if(bikeCount){
                     if(bikeCount = 1){
                         conv.close(`Momentan ist ${bikeCount} Rad bei Sievekingalle verfügbar.`);
                     } else{
-                    conv.close(`Momentan sind ${bikeCount} Räder bei Sievekingalle verfügbar.`);
+                        conv.close(`Momentan sind ${bikeCount} Räder bei Sievekingalle verfügbar.`);
                     }
                 } else {
                     conv.close('Leider sind gerade keine Räder bei Sievekingsallee verfügbar.');
                 }
-
                 resolve();
             })
             .catch( (error) => {
-                // eslint-disable-next-line max-len
                 conv.close(`Irgendwas lief schief beim Verbinden mit den Stadtrad Servern. Bitte probier es später nochmal.`);
                 resolve();
             } );
